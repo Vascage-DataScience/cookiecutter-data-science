e99d222ee920ccf4c7203446029292a1e85c52df
Kevin Baur <kevin.baur@vascage.at>
2024-12-16 09:07:02 +0100

    Adding missing main-file and app.yaml file

---
+++ {{ cookiecutter.repo_name }}/app/__main__.py
@@ -0,0 +1,8 @@
+import logging
+
+
+if __name__ == "__main__":
+    # Creates the app and test it standalone.
+    logging.info(f"Begin {__name__}")
+    # Insert Code here!
+    logging.info(f"End {__name__}")
